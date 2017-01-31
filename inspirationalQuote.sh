b8ada714821c78f25e1ea3165bf3fbfa3dd8a327
Manuel Romero <mrm8488@gmail.com>
2017-01-31 16:25:52 +0100

    Add the script to receive the inspirational quote

---
+++ inspirationalQuote.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+web='http://www.values.com/inspirational-quotes'
+page=`shuf -i 1-200 -n 1`
+quote=`curl -s $web?page=$page | grep "<h6>" | tr -s ">" | cut -f3 -d ">" | sed 's/<.*//' | shuf -n 1`
+
+echo "Daily quote: <<$quote>>"
+
+# Made by Manuel Romero - mrm8488@gmail.com
