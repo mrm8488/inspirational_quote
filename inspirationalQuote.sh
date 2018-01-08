0322454163b7003c8e6f41f13579f5b8a35328d0
Manuel Romero <mrm8488@gmail.com>
2018-01-08 21:14:20 +0100

    Change page number max from 200 to 50

--- inspirationalQuote.sh
+++ inspirationalQuote.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 web='https://www.passiton.com/inspirational-quotes'
-page=`shuf -i 1-200 -n 1`
+page=`shuf -i 1-50 -n 1`
 quote=`curl -s $web?page=$page | grep "<img alt=" | tr -s ">" | cut -f2 -d ">" | cut -f1 -d "#" | grep -v "&" | cut -f2 -d "=" | tr -d '"' | shuf -n 1`
 
 echo "Daily quote: <<$quote>>"
