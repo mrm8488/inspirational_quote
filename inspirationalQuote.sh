df40bb33427b3c18b2fc9a3ca980782e5b010314
Manuel Romero <mrm8488@gmail.com>
2018-01-08 21:09:30 +0100

    Fix: web and commands pipeline changed

--- inspirationalQuote.sh
+++ inspirationalQuote.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-web='https://www.values.com/inspirational-quotes'
+web='https://www.passiton.com/inspirational-quotes'
 page=`shuf -i 1-200 -n 1`
-quote=`curl -s $web?page=$page | grep "<h6>" | tr -s ">" | cut -f3 -d ">" | sed 's/<.*//' | shuf -n 1`
+quote=`curl -s $web?page=$page | grep "<img alt=" | tr -s ">" | cut -f2 -d ">" | cut -f1 -d "#" | grep -v "&" | cut -f2 -d "=" | tr -d '"' | shuf -n 1`
 
 echo "Daily quote: <<$quote>>"
 
