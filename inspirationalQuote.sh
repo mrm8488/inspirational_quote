33b0cf027a2e73715d6729c931ec3bbcd2b4c745
Manuel Romero <mrm8488@gmail.com>
2018-01-12 03:18:22 +0100

    Change commands pipeline
    Change commands pipeline to be adapted to new web format

--- inspirationalQuote.sh
+++ inspirationalQuote.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
+# target web
 web='https://www.passiton.com/inspirational-quotes'
+
+# random (1-50) number for pagination
 page=`shuf -i 1-50 -n 1`
+
+# magic! (filter web elements to get a quote)
 quote=`curl -s $web?page=$page | grep "<img alt=" | tr -s ">" | cut -f2 -d ">" | cut -f1 -d "#" | grep -v "&" | cut -f2 -d "=" | tr -d '"' | shuf -n 1`
 
 echo "Daily quote: <<$quote>>"
