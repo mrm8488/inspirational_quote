2bc8fdabd84d30553b5dbc7135548c58bd4a6fe2
Manuel Romero <mrm8488@gmail.com>
2018-01-12 03:58:19 +0100

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-quote=`cat webpage | grep "<img alt=" | tr -s ">" | cut -f2 -d ">" | cut -f1 -d "#" | grep -v "&" | cut -f2 -d "=" | tr -d '"' | shuf -n 1`
+quote=`cat webpage | grep "<img alt=" | tr -s ">" | cut -f2 -d ">" | cut -f1 -d "#" | cut -f2 -d '"' | grep -v "&" | shuf -n 1`
 
 if [ -n "$quote" ]
 then
