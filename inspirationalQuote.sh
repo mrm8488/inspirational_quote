2df27801559c988b8f30f8dfca63bca51bbb31f0
Manuel Romero <mrm8488@gmail.com>
2018-01-12 03:57:31 +0100

    Update inspirationalQuote.sh

--- inspirationalQuote.sh
+++ inspirationalQuote.sh
@@ -7,7 +7,7 @@ web='https://www.passiton.com/inspirational-quotes'
 page=`shuf -i 1-50 -n 1`
 
 # magic! (filter web elements to get a quote)
-quote=`curl -s $web?page=$page | grep "<img alt=" | tr -s ">" | cut -f2 -d ">" | cut -f1 -d "#" | grep -v "&" | cut -f2 -d "=" | tr -d '"' | shuf -n 1`
+quote=`curl -s $web?page=$page | grep "<img alt=" | tr -s ">" | cut -f2 -d ">" | cut -f1 -d "#" | cut -f2 -d '"' | grep -v "&" | shuf -n 1`
 
 echo "Daily quote: <<$quote>>"
 
